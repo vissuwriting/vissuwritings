c6da7bab4ca546b9e6a376329eddfebb191adfea
Medidi Murty <satya.ioscreates@gmail.com>
2026-03-06 10:09:57 +0530

    Added AV Xcode project

---
+++ AV/AV/AVApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVApp.swift
+//  AV
+//
+//  Created by Satvik on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
